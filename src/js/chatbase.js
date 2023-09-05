c347964656a3dcd0c80b757051672bd155586902
yongsin0129Laptop <yongsin@gmail.com>
2023-09-05 10:13:54 +0800

    feat : chatbase AI service finished

---
+++ src/js/chatbase.js
@@ -0,0 +1,15 @@
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment'
+
+if (ExecutionEnvironment.canUseDOM) {
+
+  /********************************************************************************
+  *
+            因為使用 injectHtmlTags 的方式，這邊的 code 就留下來，等日後需要寫 ExecutionEnvironment 時再使用
+  *
+  *********************************************************************************/
+
+  // console.log("hello , chatBase start !!")
+  // window.chatbaseConfig = {
+  //   chatbotId: "V5_rBtZ9J8a58fjU1T-sf",
+  // }
+}
