e451fa97077fb50384bdbef418600f6b26c97621
yongsin0129Laptop <yongsin@gmail.com>
2023-10-13 09:52:45 +0800

    feat : add GTM Id to head and body

--- src/js/injectHtmlTags.js
+++ src/js/injectHtmlTags.js
@@ -21,6 +21,15 @@ module.exports = function (context, options) {
               defer: true
             }
           },
+          `
+            <!-- Google Tag Manager -->
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WL8S9N4R');</script>
+            <!-- End Google Tag Manager -->
+          `
         ],
         preBodyTags: [
           {
@@ -30,7 +39,13 @@ module.exports = function (context, options) {
               style: 'display:none'
             },
             innerHTML: 'This is a custom pre-body tag.'
-          }
+          },
+          `
+          <!-- Google Tag Manager (noscript) -->
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WL8S9N4R"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+          <!-- End Google Tag Manager (noscript) -->
+          `
         ],
         postBodyTags: [
           {
